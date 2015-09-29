afcee23d068425a453f1664a43004c39a5f0e635
Chris Norman <chris.norman2@googlemail.com>
2015-09-29 12:02:34 +0100

    Made into a proper package.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+ name = 'invisible_ui',
+ version = '1.3',
+ description = 'Accessible UI elements for pygame.',
+ url = 'http://github.com/chrisnorman7/invisible_ui.git',
+ author = 'Chris Norman',
+ author_email = 'chris.norman2@googlemail.com',
+ license = 'GPL',
+ packages = ['invisible_ui'],
+ zip_safe = True,
+ keywords = ['ui', 'pygame', 'accessible', 'a11y', 'accessible', 'spoken', 'menu', 'element'],
+ install_requires = [
+  'pygame'
+ ],
+ dependency_links = [
+  'hg+http://hg.q-continuum.net/accessible_output2#egg=accessible_output2'
+ ]
+)
